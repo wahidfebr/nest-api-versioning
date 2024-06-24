39427940a403bfc46576709e84795ff3b240a221
Wahid Febrilianto - Backend <wahid@qoin.id>
2024-06-24 10:42:33 +0700

    feat: add user module with api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const HOST = 'localhost';
+  const PORT = 3000;
+
+  // Express HTTP Server
+  // const app = await NestFactory.create(AppModule);
+
+  // Fastify HTTP Server
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  await app.listen(PORT, HOST, () => {
+    console.log(`NEST running at http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
---
+++ src/modules/user/v2/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller({ path: 'users', version: '2' })
+export class UserController {
+  constructor(private readonly service: UserService) {}
+
+  @Get()
+  async hello() {
+    const data = await this.service.hello();
+    return {
+      message: 'success',
+      data: data,
+    };
+  }
+}
